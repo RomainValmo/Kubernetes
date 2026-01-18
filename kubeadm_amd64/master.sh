1edd5642bb8847415435fde5303cfda9e963db54
RomainValmo <142975520+RomainValmo@users.noreply.github.com>
2026-01-18 02:01:53 +0100

    Change modprobe to use absolute path
    Update modprobe commands to use absolute path.

--- kubeadm_amd64/master.sh
+++ kubeadm_amd64/master.sh
@@ -74,8 +74,8 @@ cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-sudo modprobe overlay
-sudo modprobe br_netfilter
+/sbin/modprobe overlay
+/sbin/modprobe br_netfilter
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
